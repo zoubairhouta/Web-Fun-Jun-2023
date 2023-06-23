d99b4d705a8fbeba1a33f2d19aaca823f81e8817
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-06-23 16:16:29 +0100

    Loop Challenges

--- Week1/Day 5/Practice/Loop Challenges/looping.js
+++ Week1/Day 5/Practice/Loop Challenges/looping.js
@@ -25,5 +25,39 @@ console.log(decreasingMultiplesOf3());
 
 // Print the sequence
 function PrintTheSequence()
-while
+
+{
+    for (var i = 0; i <= 5; i++) {
+        var value = 4 - i * 1.5;
+        console.log(value);
+      };
+}
 console.log(PrintTheSequence());
+
+// Sigma 
+function Sigma()
+{
+    var sum =0;
+    for(var i = 0;i<101;i++)
+    {
+       
+        sum+=i; 
+    }
+    return sum;
+
+}
+console.log(Sigma());
+
+// Factorial
+function Factorial()
+{
+var product = 1;
+for(var i = 1;i<13;i++)
+{
+
+    product *=i;
+
+}
+return product;
+}
+console.log(Factorial());
